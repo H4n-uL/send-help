6802213ff869fdb72815b6b994d6eb8e1a74a0dd
H4n-uL <jun061119@gmail.com>
2025-06-23 20:45:48 +0900

    j

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,6 +18,11 @@ export default defineConfig({
         target: 'http://localhost:15009',
         changeOrigin: true,
         secure: false,
+      },
+      '/uploads': {
+        target: 'http://localhost:15009',
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
